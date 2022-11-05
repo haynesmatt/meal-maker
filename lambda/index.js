0ce6d45d0ad8209bc5fe82d6d1395f9776ce5f29
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2022-11-05 22:30:26 +0000

    updated lambda/index.js.

--- lambda/index.js
+++ lambda/index.js
@@ -141,9 +141,6 @@ require("dotenv").config();
 const twilio = require("twilio");
 
 function sendText() {
-    // Download the helper library from https://www.twilio.com/docs/node/install
-    // Find your Account SID and Auth Token in Account Info and set the environment variables.
-    // See http://twil.io/secure
     const accountSid = process.env.TWILIO_ACCOUNT_SID;
     const authToken = process.env.TWILIO_AUTH_TOKEN;
     const twillioNum = process.env.TWILIO_NUMBER;
