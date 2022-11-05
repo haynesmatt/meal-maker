8989a574434462bed60f7ec8e98176519a840990
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2022-11-05 22:16:14 +0000

    updated lambda/index.js.

--- lambda/index.js
+++ lambda/index.js
@@ -137,15 +137,22 @@ const ErrorHandler = {
 };
 
 function sendText() {
-      // Download the helper library from https://www.twilio.com/docs/node/install
-      // Find your Account SID and Auth Token in Account Info and set the environment variables.
-      // See http://twil.io/secure
-      const accountSid = process.env.TWILIO_ACCOUNT_SID;
-      const authToken = process.env.TWILIO_AUTH_TOKEN;
-      const client = require('twilio')(accountSid, authToken);
-      client.messages 
-      .create({body: 'Hi there', from: '+12513223091', to: '+16463937873'})
-      .then(message => console.log(message.sid));
+      require("dotenv").config();
+const twilio = require("twilio");
+
+// Download the helper library from https://www.twilio.com/docs/node/install
+// Find your Account SID and Auth Token in Account Info and set the environment variables.
+// See http://twil.io/secure
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+const twillioNum = process.env.TWILIO_NUMBER;
+const sendToNum = process.env.SEND_TO_NUMBER;
+const client = require("twilio")(accountSid, authToken, twillioNum, sendToNum);
+
+client.messages
+  .create({ body: "Hi there", from: twillioNum, to: sendToNum })
+  .then((message) => console.log(message.sid));
+
   }
 
 /**
